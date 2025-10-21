9ff312ebf33b224a24d276df0e3afbb2fa42247d
Mateus <mateusromani@gmail.com>
2025-10-21 10:42:13 -0300

    Revert "delete initial dockerfile"
    This reverts commit fb6a0a69e6a7d17dc6511a5d0d178b4905a10cd5.

--- src/persistence/init.sql
+++
@@ -1,7 +0,0 @@
--- Optional initialization SQL script for MySQL
--- This script creates the `todo_items` table if it doesn’t exist.
-CREATE TABLE IF NOT EXISTS todo_items (
-  id VARCHAR(36) PRIMARY KEY,
-  name VARCHAR(255) NOT NULL,
-  completed BOOLEAN DEFAULT FALSE
-);
