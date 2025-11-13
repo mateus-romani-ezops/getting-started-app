354f772f641a7e58b3819a512c100887c3cae9f5
Mateus <mateusromani@gmail.com>
2025-11-13 13:29:54 -0300

    delete temporary files

--- terraform/files_working/modules/alb/variables.tf
+++
@@ -1,6 +0,0 @@
-variable "name" {}
-variable "vpc_id" {}
-variable "subnet_ids" { type = list(string) }
-variable "lb_sg_id" {}
-variable "target_port" { default = 8080 }
-variable "health_check_path" { default = "/" }
